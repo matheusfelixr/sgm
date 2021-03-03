3ec91e9c0028053b5217b49e118733b980868997
Matheus Felix Ribeiro <matheusfelixr@hotmail.com>
2021-03-02 23:03:35 -0300

    main

--- src/main/java/com/matheusfelixr/sgm/model/domain/HistoryAuthentication.java
+++ src/main/java/com/matheusfelixr/sgm/model/domain/HistoryAuthentication.java
@@ -3,14 +3,16 @@ package com.matheusfelixr.sgm.model.domain;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Date;
 
 @Data
 @Entity
 @Table(name = "HISTORY_AUTHENTICATION" )
 @SequenceGenerator(name = "SEQ_HISTORY_AUTHENTICATION", sequenceName = "SEQ_HISTORY_AUTHENTICATION", allocationSize = 1)
-public class HistoryAuthentication {
+public class HistoryAuthentication implements Serializable {
 
+    private static final long serialVersionUID = -1L;
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_HISTORY_AUTHENTICATION")
--- src/main/java/com/matheusfelixr/sgm/service/GenerateClassService.java
+++ src/main/java/com/matheusfelixr/sgm/service/GenerateClassService.java
@@ -12,18 +12,42 @@ public class GenerateClassService {
 
     public void generateClass() throws IOException {
 
-        String nameClass = "MailingLayout1";
+        String nameClass = "Person";
+        String nameEntity = "person";
+
         String nameSystem = "sgm";
         String domain = "com.matheusfelixr";
 
         String directoryBase = "src/main/java/";
-        String directoryTypeClass = "/repository/";
-        String directoryFull = directoryBase + domain.replace(".", "/")+"/"+nameSystem+directoryTypeClass;
+        String directoryTypeClassEntity = "/model/domain/";
 
-        createRepository(nameClass, nameSystem, domain, directoryFull);
+        String directoryFull = directoryBase + domain.replace(".", "/")+"/"+nameSystem+directoryTypeClassEntity;
+
+        FileWriter arq = new FileWriter(directoryFull +nameClass+".java");
+        PrintWriter printWriter = new PrintWriter(arq);
+        printWriter.println("package "+ domain +"."+ nameSystem +".model.domain;");
+
+        printWriter.println("\nimport lombok.Data;");
+
+        printWriter.println("\nimport javax.persistence.*;");
+        printWriter.println("import java.io.Serializable;");
+
+        printWriter.println("\n@Data");
+        printWriter.println("@Entity");
+        printWriter.println("@Table(name = \""+nameEntity.toUpperCase()+"\" )");
+
+
+
+        arq.close();
+
+
+        String directoryTypeClassRepository = "/repository/";
+      //  createRepository(nameClass, nameSystem, domain, directoryBase, directoryTypeClassRepository);
     }
 
-    private void createRepository(String nameClass, String nameSystem, String domain, String directoryFull) throws IOException {
+    private void createRepository(String nameClass, String nameSystem, String domain, String directoryBase, String directoryTypeClass) throws IOException {
+
+        String directoryFull = directoryBase + domain.replace(".", "/")+"/"+nameSystem+directoryTypeClass;
 
 
         FileWriter arq = new FileWriter(directoryFull + nameClass +"Repository.java");
@@ -31,7 +55,7 @@ public class GenerateClassService {
 
         printWriter.println("package "+ domain +"."+ nameSystem +".repository;");
         printWriter.println("\nimport "+ domain +"."+ nameSystem +".model.domain."+ nameClass +";");
-        printWriter.println("\nimport org.springframework.data.jpa.repository.JpaRepository;");
+        printWriter.println("import org.springframework.data.jpa.repository.JpaRepository;");
 
         printWriter.println("\npublic interface "+ nameClass +"Repository extends JpaRepository<"+ nameClass +", Long> {");
         printWriter.println("\n}\n");
